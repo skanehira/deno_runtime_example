32c9f6675474ea02bd2cdcb06a41091ea4efae87
skanehira <sho19921005@gmail.com>
2022-10-12 18:32:24 +0900

    get result of function and deserialize the result to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
+use deno_core::v8;
 use deno_runtime::permissions::Permissions;
 use deno_runtime::{deno_core, BootstrapOptions};
 use std::{rc::Rc, sync::Arc};
@@ -45,13 +47,50 @@ async fn run_js(file_path: &str) -> Result<(), AnyError> {
         stdio: Default::default(),
     };
 
-    let mut worker = deno_runtime::worker::MainWorker::bootstrap_from_options(
+    let mut runtime = deno_runtime::worker::MainWorker::bootstrap_from_options(
         main_module.clone(),
         Permissions::allow_all(),
         options,
-    );
+    )
+    .js_runtime;
 
-    worker.execute_main_module(&main_module).await
+    let module_id = runtime
+        .load_main_module(
+            &main_module,
+            Some(r#"export const obj = {name: "gorilla"}; console.log("name:", name); "#.into()),
+        )
+        .await?;
+
+    let global_scope = runtime.get_module_namespace(module_id).unwrap();
+    {
+        {
+            let handle_scope = &mut runtime.handle_scope();
+            let local_scope = v8::Local::<v8::Object>::new(handle_scope, &global_scope);
+
+            let key = serde_v8::to_v8(handle_scope, "name")?;
+            let value = serde_v8::to_v8(handle_scope, "gorilla")?;
+            local_scope.set(handle_scope, key, value);
+        }
+
+        let _ = runtime.mod_evaluate(module_id);
+        runtime.run_event_loop(false).await?;
+    }
+
+    let scope = &mut runtime.handle_scope();
+    let local_object = v8::Local::<v8::Object>::new(scope, global_scope);
+
+    let default_export_name = v8::String::new(scope, "obj").unwrap();
+    let binding = local_object.get(scope, default_export_name.into());
+    let object = binding.context("not found obj")?;
+
+    #[derive(Debug, Default, serde::Deserialize)]
+    pub struct Object {
+        pub name: Option<String>,
+    }
+
+    let obj: Object = serde_v8::from_v8(scope, object).unwrap();
+    dbg!(obj);
+    Ok(())
 }
 
 fn main() {
